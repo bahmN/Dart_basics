776e9ec680596629bc8be8ff4b9efb7f43221c7d
bahmN <iwanaev23@gmail.com>
2022-11-28 19:24:04 +0300

    ..

--- bin/nod_and_nok.dart
+++ bin/nod_and_nok.dart
@@ -1,12 +1,19 @@
+// Решение первого задания "НОД и НОК"
 class Division {
+  void res(int a, int b) {
+    print('Задание 1. Найти НОД и НОК:');
+    print(
+        'НОД($a, $b) = ${nodMethod(a, b)} \nНОК($a, $b) = ${nokMethod(a, b)}');
+  }
+
   int nodMethod(int a, int b) => a.gcd(b);
 
-  num nokMethod(int a, int b) {
-    int nodMethod(int a, int b) => a.gcd(b);
+  int nokMethod(int a, int b) {
+    int nod(int a, int b) => a.gcd(b);
     if ((a == 0) || (b == 0)) {
       return 0;
     }
 
-    return ((a ~/ nodMethod(a, b)) * b).abs();
+    return ((a ~/ nod(a, b)) * b).abs();
   }
 }
