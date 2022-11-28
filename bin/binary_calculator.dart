9e25f4ea03b7b15eeb47b6a1bd91b2b1cd5e7fe8
bahmN <iwanaev23@gmail.com>
2022-11-28 19:23:39 +0300

    Second question ready

---
+++ bin/binary_calculator.dart
@@ -0,0 +1,35 @@
+// Решение второго задания "Преобразования целых чисел из десятичной системы в двоичную и обратно"
+class BinaryCalc {
+  void res(int a, List<int> b) {
+    print(
+        '\nЗадание 2. Преобразования целых чисел из десятичной системы в двоичную и обратно:');
+    print('Число $a в двоичной системе счислений = ${calcBinary(a)}');
+    var resb = '';
+
+    for (var element in b) {
+      resb = resb + element.toString();
+    }
+
+    print(
+        'Число $resb в двоичной системе счисления = числу ${calcDecimal(b)} в десятеричной системе счисления');
+  }
+
+  String calcBinary(int a) {
+    var result = '';
+    while (a > 0) {
+      result = (a % 2).toString() +
+          result; // Получаем остаток от деления и записываем его в строку
+      a = a ~/ 2;
+    }
+    return result;
+  }
+
+  int calcDecimal(List<int> binary) {
+    var res = 0;
+    for (var bin in binary) {
+      res <<= 1;
+      res |= bin;
+    }
+    return res;
+  }
+}
--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,8 +1,12 @@
+import 'binary_calculator.dart';
 import 'nod_and_nok.dart';
 
 void main(List<String> arguments) {
-  print('Задание 1. Найти НОД и НОК:');
-  var result = Division();
-  print(
-      'НОД(147,224) = ${result.nodMethod(147, 224)} \nНОК(24, 52) = ${result.nokMethod(24, 52)}');
+  // Задание 1. НОД и НОК
+  var division = Division();
+  division.res(122, 321);
+
+  // Задание 2. Перевод из десятичной в двоичную
+  var binary = BinaryCalc();
+  binary.res(312, [1, 0, 1]);
 }
